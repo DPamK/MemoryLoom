c5ee4c2c39f4e04209c509b0b23dc2895e2280c2
wangyifei15 <wangyifei15@xiaomi.com>
2025-02-28 20:07:01 +0800

    feat(agent): 支持多条记录输入和输出，优化记录和总结代理

--- memoryloom/agent.py
+++ memoryloom/agent.py
@@ -1,3 +1,7 @@
+
+import os
+import re
+import json
 from langfuse import Langfuse
 from memoryloom.AlCaller import AICaller
 from pydantic import BaseModel, Field
@@ -36,6 +40,7 @@ class BaseAgent(ABC):
 
     def validate_response(self, response: str):
         try:
+            response = self.extract_json(response)
             obj = self.response.model_validate_json(response)
             return obj
         except Exception as e:
@@ -106,9 +111,20 @@ class BaseAgent(ABC):
     def set_response(self, response: BaseModel):
         self.response = response
 
+    def extract_json(self, text: str):
+        try:
+            if json.loads(text):
+                return [text]
+        except json.JSONDecodeError:
+                pass
+        
+        pattern = r'```(?:json)?\s*\n?(.*?)\n?```'
+        matches = re.findall(pattern, text, re.DOTALL)
+        return matches
+
 class RecordResponse(BaseModel):
     think:str = Field(..., description="思考过程")
-    record:str = Field(..., description="记录内容")
+    record:list[str] = Field(..., description="记录内容")
 
 class RecordAgent(BaseAgent):
     def __init__(self, ai_caller: AICaller, 
@@ -117,59 +133,79 @@ class RecordAgent(BaseAgent):
                  prompt_id: str = None,):
         super().__init__(ai_caller, prompt, prompt_type, prompt_id)
         self.response = RecordResponse
+
     def get_agent_name(self):
         return "record"
     
     def cls_prompt(self):
         self.__prompt_id__ = "record"
-        self.__prompt_type__ = "text"
-        self.__prompt__ = """请你根据如下的聊天记录：
-{{record}}
-站在 {{user}} 的视角，用一段文字总结出发生了什么事
-最后输出的格式请按照这个json schema输出：
-{{output_schema}}
-"""
-
-    def get_prompt(self, record: Message|str, user_name:str):
+        self.__prompt_type__ = "chat"
+        self.__prompt__ = [
+            {"role": "system", "content": """你是一个记录助手，无论用户说什么，你的工作都是将用户的话归纳总结。
+             要求如下：
+             1. 你最好能够站在 [{{user}}] 的视角，来总结出发生了什么事，但如果[]中为空或是其它无意义的值，就忽略这个要求。
+             2. 总结需准确传达原文核心观点和关键细节，内容完整且重点突出。语言简洁明了，逻辑连贯，避免添加主观臆断和无关信息。
+             3. 当用户信息为空或者其它无意义的值时，输出记录可以为空的list。
+             4. 如果只用一条记录无法涵盖所有重要信息，可以给出多条记录。
+             5. 最后输出的格式请按照这个json schema输出：
+             {{output_schema}}"""},
+            {"role": "user", "content": "{{input_history}}"}
+        ]
+    
+    def get_prompt(self, record: Message|str, user_name:str=""):
         prompt = self.langfuse_client.get_prompt(self.prompt_id)
         compiled_prompt = prompt.compile(
-            record = self.process_history(record),
-            user_name = user_name,
+            input_history = self.process_history(record),
+            user = user_name,
             output_schema = self.response.model_json_schema()
         )
         return compiled_prompt
 
-class DayResponse(BaseModel):
+class SummaryResponse(BaseModel):
     think:str = Field(..., description="思考过程")
-    record:str = Field(..., description="记录内容")
-    long_memory:list[str] = Field(..., description="需要长期记录的内容")
+    summary:str = Field(..., description="总结内容")
 
-class DayAgent(BaseAgent):
+class SummaryAgent(BaseAgent):
     def __init__(self, ai_caller: AICaller, 
                  prompt: str = None,
                  prompt_type: Literal["text", "chat"] = None,
                  prompt_id: str = None,):
         super().__init__(ai_caller, prompt, prompt_type, prompt_id)
-        self.response = DayResponse
+        self.response = SummaryResponse
+
     def get_agent_name(self):
-        return "day"
+        return "summary"
     
     def cls_prompt(self):
-        self.__prompt_id__ = "day"
-        self.__prompt_type__ = "text"
-        self.__prompt__ = """请你根据如下的事件记录：
-{{record}}
-用一段文字总结这一天的情况，
-对于需要长期记忆的内容，请另外列出需要记忆的文本，每一项单独列出
-最后输出的格式请按照这个json schema输出：
-{{output_schema}}
-"""
+        self.__prompt_id__ = "summmary"
+        self.__prompt_type__ = "chat"
+        self.__prompt__ = [
+            {"role": "system", "content": """你是一个内容归纳助手，无论用户说什么，你的工作都是将用户的话归纳总结为一段文本。
+             要求如下：
+             1. 总结需准确传达原文核心观点和关键细节，内容完整且重点突出。语言简洁明了，逻辑连贯，避免添加主观臆断和无关信息。
+             2. 丢弃掉不重要的信息，总结更重要的信息。
+             3. 最后输出的格式请按照这个json schema输出：
+             {{output_schema}}"""},
+            {"role": "user", "content": "{{input_history}}"}
+        ]
 
-    def get_prompt(self, record: Message|str):
+    def get_prompt(self, origin_text:str):
         prompt = self.langfuse_client.get_prompt(self.prompt_id)
         compiled_prompt = prompt.compile(
-            record = self.process_history(record),
+            input_history = origin_text,
             output_schema = self.response.model_json_schema()
         )
         return compiled_prompt
+
+if __name__ == "__main__":
+    llm = AICaller(
+        model_name=os.environ.get("LLM_MODEL"),
+        api_token=os.environ.get("LLM_API_KEY"),
+        api_base=os.environ.get("LLM_API_BASE"),
+        proxy=os.environ.get("LLM_PROXIES"),
+    )
+    recoder = RecordAgent(llm)
+    res = recoder(record = "你好，我叫张三", user_name="张三")
+    print(res)
+
 
